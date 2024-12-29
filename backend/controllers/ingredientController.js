d18bdb4e724e142746f422e8f6adf1922fa59e83
HelicopterNguyen <itcsiu23030@student.hcmiu.edu.vn>
2024-12-29 21:54:41 +0700

    delete the redundant backend folder

--- backend/controllers/ingredientController.js
+++ backend/controllers/ingredientController.js
@@ -2,8 +2,15 @@ import ingredientModel from "../models/ingredientModel.js";
 
 export const addIngredient = async (req, res) => {
   try {
-    const { name, quantity, unit, price, image } = req.body; 
-    const newIngredient = await ingredientModel.create({ name, quantity, unit, price, image });
+    const { name, quantity, unit, price } = req.body;
+    const image = req.file ? `/uploads/${req.file.filename}` : null;
+    const newIngredient = await ingredientModel.create({
+      name,
+      quantity,
+      unit,
+      price,
+      image,
+    });
     res.status(201).json({ success: true, data: newIngredient });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
@@ -22,8 +29,9 @@ export const getAllIngredients = async (req, res) => {
 export const updateIngredient = async (req, res) => {
     const { ingredientId } = req.params;
     const updates = req.body;
+  
     if (req.file) {
-      updates.image = req.file.path; 
+      updates.image = `/uploads/${req.file.filename}`;
     }
   
     try {
@@ -32,17 +40,21 @@ export const updateIngredient = async (req, res) => {
         updates,
         { new: true }
       );
+  
       if (!updatedIngredient) {
         return res.status(404).json({ error: "Ingredient not found" });
       }
   
-      res.status(200).json({ message: "Ingredient updated successfully", ingredient: updatedIngredient });
+      res.status(200).json({
+        message: "Ingredient updated successfully",
+        ingredient: updatedIngredient,
+      });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   };
   
-  export const deleteIngredient = async (req, res) => {
+export const deleteIngredient = async (req, res) => {
     const { ingredientId } = req.params; 
     try {
       const deletedIngredient = await ingredientModel.findByIdAndDelete(ingredientId);
--- src/backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://helicopter:Nhan123@cluster0.gxpzb.mongodb.net/OOAD_project?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"))
-}
